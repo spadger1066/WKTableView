49c22908b3a78f870f509bacc0fab7f02d3ab6ad
Stephen Maitland <s.maitland@me.com>
2015-10-20 06:37:35 +0100

    Initial Commit

---
+++ WKTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKTableView
+//
+//  Created by Stephen on 20/10/2015.
+//  Copyright © 2015 lumiator.technology.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
